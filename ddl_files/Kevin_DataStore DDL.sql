e26989296668605bbef146757cb573aa14199d66
Kevin <kevin.lin@appian.com>
2019-11-07 11:16:44 -0500

    Committed by kevinlin980

---
+++ ddl_files/Kevin_DataStore DDL.sql
@@ -0,0 +1,19 @@
+/*
+Data Store: Kevin_DataStore
+Created: 2019-11-07T15:50:45.620Z
+By: kevin.lin.s
+Appian Version: 20.1.0.50
+Target Database: Microsoft SQL Server 14.00.3223
+Database Driver: Microsoft JDBC Driver 7.0 for SQL Server 7.0.0.0
+*/
+
+/* UPDATE DDL */
+/* DROP AND CREATE DDL */
+/* WARNING: The DDL commented out below will drop and re-create all tables.
+    drop table [kevindata];
+
+    create table [kevindata] (
+        [id] int not null,
+        primary key ([id])
+    );
+*/
